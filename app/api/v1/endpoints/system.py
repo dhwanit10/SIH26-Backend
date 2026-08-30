5082b1e1748f5b23d211def5854abeb8bdff05f3
dhwanit10 <dhwanitakoliya10@gmail.com>
2026-08-30 11:23:05 +0530

    added get-offline system api

--- app/api/v1/endpoints/system.py
+++ app/api/v1/endpoints/system.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session, joinedload
 
 from app.core.database import get_db
-from app.models.system import System
+from app.models.system import System, SystemStatus
 from app.schemas.system import SystemsResponse
 
 router = APIRouter()
@@ -17,6 +17,25 @@ async def get_systems(
         .options(
             joinedload(System.sessions)
         )
+        .order_by(System.id)
+        .all()
+    )
+
+    return SystemsResponse(
+        total=len(systems),
+        systems=systems
+    )
+
+@router.get("/get-offline", response_model=SystemsResponse)
+async def get_systems(
+    db: Session = Depends(get_db)
+):
+    systems = (
+        db.query(System)
+        .options(
+            joinedload(System.sessions)
+        )
+        .filter(System.status == SystemStatus.OFFLINE)
         .order_by(System.id)
         .all()
     )
